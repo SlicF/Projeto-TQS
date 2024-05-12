c770fe0bf2546d398496d2459a6ca897da51d379
Gabriel Teixeira <gabrielm.teixeira@ua.pt>
2024-05-12 17:57:20 +0100

    incio do Spring boot Tickets

---
+++ airportManager/src/main/java/ua/tqs/airportManager/repository/TicketRepository.java
@@ -0,0 +1,18 @@
+package ua.tqs.airportManager.repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import ua.tqs.airportManager.model.Ticket;
+
+@Repository
+public class TicketRepository {
+
+
+    private List<Ticket> ticketDatabase = new ArrayList<>();
+
+    // adicionar funcoes de ticket
+    
+}
